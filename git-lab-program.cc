be8b8f93840b8bcc6cbe5a97595d431f203ee332
Aaron Mapes <am429922@ohio.edu>
2025-05-14 21:32:05 -0400

    Complete file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Aaron Mapes
+ *        Date: 5/14/25
+ * Description: Hello to Git!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
